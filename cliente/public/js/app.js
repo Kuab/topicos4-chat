2901922f0676523a4f25dd9ce0e5bdc1b8cf77b6
Vinicius Esperanca <vinicius-esperanca@kuab.com.br>
2013-10-30 08:56:47 -0200

    Versao finalizada

--- cliente/public/js/app.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-
-// Declara um novo app chamado 'myApp', passando as configurações dos outros app's: 'myApp.filters', ''myApp.drectives e 'luegg.directives'
-var app = angular.module('myApp', []);
